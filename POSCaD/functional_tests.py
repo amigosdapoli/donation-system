265e606b78c9d681d57ab702b494fa566ade4930
Fernando Paiva <fernandosjp@gmail.com>
2017-07-23 21:57:37 -0300

    Adds donation form page functional test

--- POSCaD/functional_tests.py
+++ POSCaD/functional_tests.py
@@ -4,14 +4,17 @@ import unittest
 class NewDonorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_form_url(self):
-        # just checks if the url is correct
-        self.broswer.get('http://localhost:8000/doar')
+        # Donor has heard about the opportunity to donate to the organization and enters the website
+        self.browser.get('http://localhost:8000/doar/donation_form')
 
-if __name__== '__main__:':
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Amigos da Poli', self.browser.title) 
+ 
+if __name__ == '__main__':
     unittest.main(warnings='ignore')
