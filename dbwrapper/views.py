eb754d04551fcab7d735313a345d2e94c49681b7
carvmarc <marcos.carvalho@gorilainvest.com.br>
2017-08-16 11:00:14 +0000

    closes amigosdapoli/donation-system/#13, amigosdapoli/donation-system/#14

--- dbwrapper/views.py
+++ dbwrapper/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework import viewsets
 from .models import Donor, Donation
 from .serializers import DonorSerializer, DonationSerializer
-
+import json
 class DonorViewSet(viewsets.ModelViewSet):
     queryset = Donor.objects.all().order_by('CPF')
     serializer_class = DonorSerializer
@@ -12,5 +12,29 @@ class DonationViewSet(viewsets.ModelViewSet):
     queryset = Donation.objects.all().order_by('value')
     serializer_class = DonationSerializer
 
+
 def donation_form(request):
-	return render(request, 'dbwrapper/donation_form.html', {})
+    if request.method == 'POST':
+        tax_id = request.POST.get('donor_tax_id')
+        
+        # tax id is required
+        if not tax_id:
+            raise Exception('tax_id need to be provided')
+        donor = Donor.objects.filter(tax_id=tax_id).first()
+
+        # creates  a new donor
+        if not donor:
+            new_donor = Donor()
+            new_donor.tax_id = tax_id
+            new_donor.name = request.POST.get('donor_name')
+            new_donor.surname = request.POST.get('donor_surname')
+            new_donor.save()
+            donor = new_donor
+
+        # Donation
+        new_donation = Donation()
+        new_donation.value = request.POST.get('donation_value')
+        new_donation.donor_tax_id = donor.tax_id
+        new_donation.recurring = False
+        new_donation.save()
+    return render(request, 'dbwrapper/donation_form.html', {})
