630c2df2434020dde4da8c05b50cbafc6963dcdc
Ricardo Numakura <ricardo.numakura@gmail.com>
2017-08-29 22:40:40 -0300

    Changed form and donation_form.html to use Django forms

--- dbwrapper/urls.py
+++ dbwrapper/urls.py
@@ -9,5 +9,5 @@ from rest_framework import routers
 urlpatterns = [
     #url(r'^', include(router.urls)),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^$', views.donation_form, name='donation_form'),
+    url(r'^', views.donation_form, name='donation_form'),
 ]
