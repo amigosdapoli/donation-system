7f07bc6bf717f62e97ac77d9b3159d40e66b0fbc
Fernando Salaroli <fernando@salaro.li>
2017-09-12 22:26:47 -0300

    livereload + gitignore + staticfiles

--- POSCaD/settings/base.py
+++ POSCaD/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'rest_framework',
 ]
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'POSCaD.urls'
