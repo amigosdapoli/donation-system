1444bf92e7eec327c92f2171fe548bcd64f27f66
Fernando Paiva <fernandosjp@gmail.com>
2017-10-15 22:02:22 -0200

    Clean unused code

--- dbwrapper/urls.py
+++ dbwrapper/urls.py
@@ -3,7 +3,5 @@ from . import views
 from dbwrapper.views import DonationFormView
 
 urlpatterns = [
-    #url(r'^', include(router.urls)),
-    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', DonationFormView.as_view(), name='donation_form'),
 ]
