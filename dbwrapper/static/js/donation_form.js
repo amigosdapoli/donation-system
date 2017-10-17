c6bdfe4342a269d5a8f635a293ee4058764090ec
Fernando Paiva <fernandosjp@gmail.com>
2017-10-17 21:36:38 -0200

    Add conditional field for recurring donation option

---
+++ dbwrapper/static/js/donation_form.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('input[name=is_recurring]').on('change', function() {
+      if ( $('#rad2').is(':checked'))
+      {
+        $("#id_installments").show();
+      }
+      else
+      {
+        $("#id_installments").hide();
+      }
+    });
+
+    $('#id_course_taken').on('change', function() {
+      if ( this.selectedIndex != '0')
+      {
+        $("#id_course_year").show();
+      }
+      else
+      {
+        $("#id_course_year").hide();
+      }
+    });
+});
