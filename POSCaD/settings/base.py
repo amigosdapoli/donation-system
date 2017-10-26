29189878e72b25fc83d9e038d88bc7a2818ac866
Fernando Paiva <fernandosjp@gmail.com>
2017-10-25 22:54:06 -0200

    Debub turned of on base and get django key from env variable

--- POSCaD/settings/base.py
+++ POSCaD/settings/base.py
@@ -20,10 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%3gi6537usk%aei_6570kvzszell8h5vum(&q*3#5)^*h0xq8&'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','%3gi6537usk%aei_6570kvzszell8h5vum(&q*3#5)^*h0xq8&')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
--- POSCaD/settings/dev.py
+++ POSCaD/settings/dev.py
@@ -1,5 +1,8 @@
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
