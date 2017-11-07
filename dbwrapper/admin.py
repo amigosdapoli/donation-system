3d7aeb3d0bbc798130cbf4cf15e23f6740629317
Fernando Paiva <fernandosjp@gmail.com>
2017-11-07 20:25:56 -0200

    Add donations table to admin and start download to csv action

--- dbwrapper/admin.py
+++ dbwrapper/admin.py
@@ -1,13 +1,36 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Donation, Donor
+import io
+import csv
+import logging
+
+logger = logging.getLogger(__name__)
 
 admin.site.disable_action('delete_selected')
 
 class DonationAdmin(admin.ModelAdmin):
+    actions = ['download_csv']
     search_fields = ('donor_full_name', 'donor_tax_id')
     list_display = ('created_at_format', 'donor_full_name', 'donor_email', 'order_id', \
                     'nsu_id', 'donor_tax_id', 'donation_value', 'is_recurring', 'was_captured')
     list_filter = ('was_captured', )
+
+    def download_csv(self, request, queryset):
+
+        filename = 'stat-info.csv'
+        f = io.StringIO()
+        writer = csv.writer(f)
+        writer.writerow(["created_at", "order_id"])
+
+        for s in queryset:
+            writer.writerow([s.created_at, s.order_id])
+
+        f.seek(0)
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+        return response
+    download_csv.short_description = "Download CSV file for selected donations."
 
     def donor_full_name(self, instance):
         return instance.donor.name + " " + instance.donor.surname
