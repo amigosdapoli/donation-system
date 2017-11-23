f795373c9af46c9071d6b1423bf29f125b338685
Fernando Paiva <fernando@fundacaolemann.org.br>
2017-11-22 23:19:03 -0200

    Avoid part of duplicated code in test (#91)
    * Avoid part of duplicated code in test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,92 +12,14 @@ class NewDonorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_enter_donation_form_and_execute_donation(self):
-        # Donor has heard about the opportunity to donate to the organization and enters the website
-        self.browser.get(self.live_server_url)
-
-        # She notices the page title and header mention to-do lists
-        #self.assertIn('Doação', self.browser.title)
-        #header_text = self.browser.find_element_by_name('nav_bar_title').text
-        #self.assertIn('Doação', header_text)
-
-        # Donor types donation info
-        # she identifies the dropdown box as to select which type of donation she wants to do
-        # select_elm = self.browser.find_element_by_id("id_is_recurring_field")
-        # for option in select_elm.find_elements_by_tag_name('option'):
-        #     if option.text == 'Pontual':
-        #         # She chooses the "Recorrente" type donation
-        #         option.click()  # select() in earlier versions of webdriver
-        #         break
-
+    def fill_in_donation_fields_right(self):
         # She identifies the text box to input donation value
         donation_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Valor da doação')]")
 
         # She sees a text box to input the donation value
         donation_input_box.send_keys('50')
 
-        # She starts filling in personal information
-        # First, she identifies the boxes to write her name and surname...
-        name_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Nome')]")
-        surname_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Sobrenome')]")
-
-        # ...then, writes her name...
-        name_input_box.send_keys("Maria")
-
-        # ...and surname.
-        surname_input_box.send_keys("Silva")
-
-        # Identifies the text box for her CPF...
-        CPF_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'CPF')]")
-
-        # ... and inputs her own
-        CPF_input_box.send_keys("128.164.150-23")
-
-        # Identifies the text box for her phone number...
-        phone_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Telefone')]")
-
-        # ... and inputs hers
-        phone_input_box.send_keys("11998765432")
-
-        # Identifies email text box
-        email_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'E-mail')]")
-
-        # ...And inputs hers
-        email_input_box.send_keys("fernandosjp@gmail.com")
-
-        # Inputs Payment details
-        cc_name = self.browser.find_element_by_id("id_name_on_card")
-        cc_name.send_keys("Fulano de Tal")
-        cc_number = self.browser.find_element_by_id("id_card_number")
-        cc_number.send_keys("4111111111111111")
-        cc_expire_month = self.browser.find_element_by_id("id_expiry_date_month")
-        cc_expire_month.send_keys("12")
-        cc_expire_year = self.browser.find_element_by_id("id_expiry_date_year")
-        cc_expire_year.send_keys("18")
-        cc_cvv = self.browser.find_element_by_id("id_card_code")
-        cc_cvv.send_keys("123")
-        time.sleep(5)
-
-        # Submit
-        submit = self.browser.find_element_by_name("subbtn")
-        submit.send_keys(Keys.ENTER)
-        time.sleep(5)
-
-        self.assertIn('Muito obrigado pela sua doação!', self.browser.page_source)
-
-    def test_donor_fills_wrong_info_and_gets_list_of_fields_to_correct(self):
-        """
-        I will start testing only payment info and them try to make it more generic
-        """
-        # Donor has heard about the opportunity to donate to the organization and enters the website
-        self.browser.get(self.live_server_url)
-
-        # She identifies the text box to input donation value
-        donation_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Valor da doação')]")
-
-        # She sees a text box to input the donation value
-        donation_input_box.send_keys('50')
-
+    def fill_in_personal_fields_right(self):
         # She starts filling in personal information
         # First, she identifies the boxes to write then she fill them in...
 
@@ -113,20 +35,46 @@ class NewDonorTest(LiveServerTestCase):
         surname_input_box.send_keys("Silva")
         CPNJ_input_box.send_keys("128.164.150-23")
         phone_input_box.send_keys("11998765432")
-        email_input_box.send_keys("fernandosjp@gmail.com")
+        email_input_box.send_keys("teste@gmail.com")
 
+    def fill_in_cc_fields(self, credit_card_number):
         # Inputs Payment details
         cc_name = self.browser.find_element_by_id("id_name_on_card")
         cc_name.send_keys("Fulano de Tal")
         cc_number = self.browser.find_element_by_id("id_card_number")
-        cc_number.send_keys("411111111111111") # Missing one number
+        cc_number.send_keys(credit_card_number)
         cc_expire_month = self.browser.find_element_by_id("id_expiry_date_month")
         cc_expire_month.send_keys("12")
         cc_expire_year = self.browser.find_element_by_id("id_expiry_date_year")
         cc_expire_year.send_keys("18")
         cc_cvv = self.browser.find_element_by_id("id_card_code")
         cc_cvv.send_keys("123")
+
+    def test_can_enter_donation_form_and_execute_donation(self):
+        # Donor has heard about the opportunity to donate to the organization and enters the website
+        self.browser.get(self.live_server_url)
+
+        self.fill_in_donation_fields_right()
+        self.fill_in_personal_fields_right()
+        self.fill_in_cc_fields("4111111111111111")
+
+        # Submit
+        submit = self.browser.find_element_by_name("subbtn")
+        submit.send_keys(Keys.ENTER)
         time.sleep(5)
+
+        self.assertIn('Muito obrigado pela sua doação!', self.browser.page_source)
+
+    def test_donor_fills_wrong_info_and_gets_list_of_fields_to_correct(self):
+        """
+        I will start testing only payment info and them try to make it more generic
+        """
+        # Donor has heard about the opportunity to donate to the organization and enters the website
+        self.browser.get(self.live_server_url)
+
+        self.fill_in_donation_fields_right()
+        self.fill_in_personal_fields_right()
+        self.fill_in_cc_fields("411111111111111") # Missing one number
 
         # Submit
         submit = self.browser.find_element_by_name("subbtn")
@@ -140,67 +88,9 @@ class NewDonorTest(LiveServerTestCase):
         # Donor has heard about the opportunity to donate to the organization and enters the website
         self.browser.get(self.live_server_url)
 
-        # She notices the page title and header mention to-do lists
-        #self.assertIn('Doação', self.browser.title)
-        #header_text = self.browser.find_element_by_name('nav_bar_title').text
-        #self.assertIn('Doação', header_text)
-
-        # Donor types donation info
-        # she identifies the dropdown box as to select which type of donation she wants to do
-        # select_elm = self.browser.find_element_by_id("id_is_recurring_field")
-        # for option in select_elm.find_elements_by_tag_name('option'):
-        #     if option.text == 'Pontual':
-        #         # She chooses the "Recorrente" type donation
-        #         option.click()  # select() in earlier versions of webdriver
-        #         break
-
-        # She identifies the text box to input donation value
-        donation_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Valor da doação')]")
-
-        # She sees a text box to input the donation value
-        donation_input_box.send_keys('50')
-
-        # She starts filling in personal information
-        # First, she identifies the boxes to write her name and surname...
-        name_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Nome')]")
-        surname_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Sobrenome')]")
-
-        # ...then, writes her name...
-        name_input_box.send_keys("Maria")
-
-        # ...and surname.
-        surname_input_box.send_keys("Silva")
-
-        # Identifies the text box for her CNPJ...
-        CPNJ_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'CPF')]")
-
-        # ... and inputs her own
-        CPNJ_input_box.send_keys("128.164.150-23")
-
-        # Identifies the text box for her phone number...
-        phone_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'Telefone')]")
-
-        # ... and inputs hers
-        phone_input_box.send_keys("11998765432")
-
-        # Identifies email text box
-        email_input_box = self.browser.find_element_by_xpath("//label[contains(text(),'E-mail')]")
-
-        # ...And inputs hers
-        email_input_box.send_keys("fernandosjp@gmail.com")
-
-        # Inputs Payment details
-        cc_name = self.browser.find_element_by_id("id_name_on_card")
-        cc_name.send_keys("Fulano de Tal")
-        cc_number = self.browser.find_element_by_id("id_card_number")
-        cc_number.send_keys("4111111111111112")
-        cc_expire_month = self.browser.find_element_by_id("id_expiry_date_month")
-        cc_expire_month.send_keys("12")
-        cc_expire_year = self.browser.find_element_by_id("id_expiry_date_year")
-        cc_expire_year.send_keys("18")
-        cc_cvv = self.browser.find_element_by_id("id_card_code")
-        cc_cvv.send_keys("123")
-        time.sleep(5)
+        self.fill_in_donation_fields_right()
+        self.fill_in_personal_fields_right()
+        self.fill_in_cc_fields("4111111111111112")
 
         # Submit
         submit = self.browser.find_element_by_name("subbtn")
