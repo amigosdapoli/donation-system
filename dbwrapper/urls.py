1858186382e3d492232c3bc028d129ab9b462a3e
Wesley Batista <wesley.batista.santos@gmail.com>
2018-03-04 15:01:29 -0300

    Add 'donation_wizard' route to dbwrapper/urls.py

--- dbwrapper/urls.py
+++ dbwrapper/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url, include
 from . import views
 from dbwrapper.views import DonationFormView
 from dbwrapper.views import StatisticsView
+from dbwrapper.views import DonationWizard
 
 urlpatterns = [
     url(r'^$', DonationFormView.as_view(), name='donation_form'),
     url(r'^statistics', StatisticsView.as_view(), name='statistics'),
+    url(r'^donation_wizard', DonationWizard.as_view(), name='donation_wizard'),
 ]
 
