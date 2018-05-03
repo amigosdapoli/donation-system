9f2b2ec15a11de977421d7ed88b2738a7a6dfdb6
Fernando Paiva <fernando@fundacaolemann.org.br>
2018-05-02 23:06:40 -0300

    Fix psql commands

--- scripts/setup_database_linux.sh
+++ scripts/setup_database_linux.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 	
-psql -Uadmin -c "CREATE DATABASE poscad;"
-psql -Uadmin -c "CREATE USER admin WITH PASSWORD 'admin' ;"
-psql -Uadmin -c "ALTER ROLE admin SET client_encoding TO 'utf8';"
-psql -Uadmin -c "ALTER ROLE admin SET default_transaction_isolation TO 'read committed';"
-psql -Uadmin -c "ALTER ROLE admin SET timezone TO 'UTC-3';"
-psql -Uadmin -c "GRANT ALL PRIVILEGES ON DATABASE poscad TO admin;"
-psql -Uadmin -c "ALTER USER admin CREATEDB;"
+psql -U admin -c "CREATE DATABASE poscad;"
+psql -U admin -c "CREATE USER admin WITH PASSWORD 'admin' ;"
+psql -U admin -c "ALTER ROLE admin SET client_encoding TO 'utf8';"
+psql -U admin -c "ALTER ROLE admin SET default_transaction_isolation TO 'read committed';"
+psql -U admin -c "ALTER ROLE admin SET timezone TO 'UTC-3';"
+psql -U admin -c "GRANT ALL PRIVILEGES ON DATABASE poscad TO admin;"
+psql -U admin -c "ALTER USER admin CREATEDB;"
