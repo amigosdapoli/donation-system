e543de5506e03cc038f50ff464edbc5e9d4af808
Henrique Kano <henriquekano@hotmail.com>
2018-06-03 12:48:40 -0300

    chore: remove duplicate code

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -194,40 +194,6 @@ class NewDonorTest(StaticLiveServerTestCase):
 
         self.wait_for(lambda: self.assertIn('Erro nas informações de cartão de crédito enviadas.', self.browser.page_source))
 
-
-    def _create_user(self):
-        self.username = "test_admin"
-        self.password = User.objects.make_random_password()
-        user, created = User.objects.get_or_create(username=self.username)
-        user.set_password(self.password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.is_active = True
-        user.save()
-        self.user = user
-
-    def test_admin_pages(self):
-        self._create_user()
-        client = Client()
-        client.login(username=self.username, password=self.password)
-        admin_pages = [
-            "/admin/",
-            # put all the admin pages for your models in here.
-            "/admin/auth/",
-            "/admin/auth/group/",
-            "/admin/auth/group/add/",
-            "/admin/auth/user/",
-            "/admin/auth/user/add/",
-            "/admin/password_change/",
-            "/admin/dbwrapper/donation/",
-            "/admin/dbwrapper/donor/",
-        ]
-        for page in admin_pages:
-            resp = client.get(page)
-            print(page)
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn(b'<!DOCTYPE html', resp.content)
-
     def test_userc_can_follow_statistics(self):
         # Donor has there is a campaign and wants to follow progress
         self.browser.get(self.live_server_url + "/statistics")
